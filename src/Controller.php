66297b98d731e49390b0ce229ca33790e2639048
Bruno Goncalves <bugotech@gmail.com>
2017-06-22 11:44:48 -0300

    controller

---
+++ src/Controller.php
@@ -0,0 +1,60 @@
+<?php namespace Bugotech\Http;
+
+use Closure;
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Bugotech\Http\Exceptions\HttpException;
+use Illuminate\Routing\Controller as BaseController;
+use Symfony\Component\HttpFoundation\Response as BaseResponse;
+
+abstract class Controller extends BaseController
+{
+    /**
+     * Transacoes.
+     *
+     * @param callable $callback
+     * @return JsonResponse|\Illuminate\Http\RedirectResponse|mixed
+     */
+    protected function transaction(Closure $callback)
+    {
+        db()->beginTransaction();
+        try {
+            $return  = $callback();
+
+            db()->commit();
+
+            return $return;
+
+        } catch (Exception $e) {
+            db()->rollBack();
+
+            return $this->exceptions($e);
+        }
+    }
+
+    /**
+     * @param Exception $e
+     * @return \Illuminate\Http\RedirectResponse|JsonResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    protected function exceptions(Exception $e)
+    {
+        if ((request()->ajax() && ! request()->pjax()) || request()->wantsJson()) {
+            $error = new \stdClass();
+            $error->code = $e->getCode();
+            $error->message = $e->getMessage();
+            return new JsonResponse($error, 422);
+        }
+
+        // Verificar se ja eh um Response
+        if ($e instanceof BaseResponse) {
+            return $e;
+        }
+
+        // Verificar se já eh um HttpException
+        if (! ($e instanceof HttpException)) {
+            $e = new HttpException(request(), $e->getMessage(), $e->getCode(), $e->getPrevious());
+        }
+
+        return $e->getResponse();
+    }
+}
---
+++ src/Exceptions/HttpException.php
@@ -0,0 +1,60 @@
+<?php namespace Bugotech\Http\Exceptions;
+
+use Exception;
+use Illuminate\Http\Request;
+
+class HttpException extends Exception
+{
+    /**
+     * @var Request
+     */
+    protected $request;
+
+    /**
+     * @param Request $request
+     * @param string $message
+     * @param int $code
+     * @param Exception $previous
+     */
+    public function __construct(Request $request, $message = "", $code = 0, Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+
+        $this->request = $request;
+    }
+
+    /**
+     * Get the underlying response instance.
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function getResponse()
+    {
+        $response = redirect()->to($this->getRedirectUrl());
+        $response->withInput($this->request->input());
+        $response->withErrors($this->getErrors(), 'default');
+        $response->setCharset(config('app.charset', 'UTF-8'));
+
+        return $response;
+    }
+
+    /**
+     * Array de erros.
+     *
+     * @return array
+     */
+    protected function getErrors()
+    {
+        return ['error' => $this->getMessage()];
+    }
+
+    /**
+     * URL de volta.
+     *
+     * @return string
+     */
+    protected function getRedirectUrl()
+    {
+        return url()->previous();
+    }
+}
